436dfdf1c874fd63653c0e7cb3acee28ee374295
chaudhryumair <chaudhryumair007@yahoo.com>
2025-06-02 19:35:06 +0500

    Project created

---
+++ ToDoList/ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Chaudhry Umair on 02/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
